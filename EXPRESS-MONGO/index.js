ff17675e13ef7a10a8671c4ce890b50e16d1a31f
99Ace <ace.ang@trentglobal.edu.sg>
2022-02-27 12:38:44 +0000

    CRUD Using Express and MongoDB

---
+++ EXPRESS-MONGO/index.js
@@ -0,0 +1,189 @@
+// ============================================
+// ============== 1. S E T  U P ===============
+// ============================================
+
+// ******************************
+// ASSIGN "require" TO VARIABLES
+// ******************************
+const express = require('express');
+const hbs = require('hbs');
+const waxOn = require('wax-on');
+const MongoUtil = require("./MongoUtil.js");
+// const axios = require('axios');
+var ObjectId = require('mongodb').ObjectId;
+const helpers = require('handlebars-helpers')(
+    {
+        'handlebars': hbs.handlebars
+    }
+);
+const dotenv = require('dotenv');
+const { Collection } = require('mongodb');
+dotenv.config();
+
+async function main() {
+    // ******************************
+    // SET UP THE REQUIREMENTS 
+    // ******************************
+
+    // 1A. SETUP EXPRESS application
+    let app = express();
+
+    // 1B. SETUP VIEW ENGINE
+    app.set('view engine', 'hbs');
+
+    // 1C. SETUP STATIC FOLDER
+    app.use(express.static('public'));
+    // set up static file for images, css
+    // in this case, we set up to link to /public folder
+
+    // 1D. SETUP WAX ON (FOR TEMPLATE INHERITANCE)
+    waxOn.on(hbs.handlebars);
+    waxOn.setLayoutPath('./views/layouts'); // set up templates inheritance path
+
+    // 1E. ENABLE FORMS
+    app.use(express.urlencoded({ extended: false }));
+
+    // 1F. Connect to Mongo
+    await MongoUtil.connect(process.env.MONGO_URI, process.env.DBNAME);
+    let db = MongoUtil.getDB();
+
+// ============================================
+// ============== 2. R O U T E S ==============
+// ============================================
+
+    // ******************************************************
+    // READ ROUTE - LOAD IN FROM MONGODB AND RENDER TO FRONTEND 
+    // ******************************************************
+    app.get('/', async function(req,res) {
+
+        // Load in from MongoDB
+        let dataInExpress = await db.collection( process.env.COLLECTION ).find().toArray();
+        console.log(dataInExpress);
+
+        // pass the data to hbs
+        res.render('index.hbs', {
+            'dataInHbs' : dataInExpress
+        });
+
+    })
+    // ******************************************************
+    // CREATE ROUTE 
+    // ******************************************************
+    
+    // .get - to send to the frontend and show the form
+    app.get('/create', function (req,res){
+        res.render('create.hbs');
+    })
+    // .post - route for the form to submit in
+    app.post('/create', function (req,res){
+
+        // retrieve the form details from the body object
+        let username = req.body.username;
+
+        // organise the data in object format
+        let newData = {
+            username : username
+        }
+
+        // save to MONGODB
+        db.collection( process.env.COLLECTION ).insertOne( newData )
+
+        // redirect route to root
+        res.redirect('/');
+    })
+    // ******************************************************
+    // UPDATE ROUTE 
+    // ******************************************************
+    
+    // .get route 
+    app.get('/:taskId/update', async function(req,res){
+
+        // retrieve the taskId from the params object
+        let taskId = req.params.taskId;
+
+        // Find in MONGODB using the taskId, using the findOne()
+        let dataInExpress = await db.collection( process.env.COLLECTION ).findOne(
+            {
+                '_id' : ObjectId( taskId )
+            }
+        );
+
+        // pass the data to hbs
+        res.render('update.hbs', {
+            'dataInHbs' : dataInExpress
+        });
+    })
+    // .post route
+    app.post('/:taskId/update', async function(req,res){
+
+        // retrieve the taskId from the params object
+        let taskId = req.params.taskId;
+
+        // retrieve the form details from the body object
+        let username = req.body.username;
+
+        let newData = {
+            username : username
+        };
+
+        // Update the data in MONGODB using the taskId and the updateOne()
+        db.collection( process.env.COLLECTION ).updateOne(
+            {
+                '_id' : ObjectId( taskId )
+            },
+            {
+                '$set' : newData
+            }
+        )
+
+        // redirect route to root
+        res.redirect('/');
+    })
+
+    // ******************************************************
+    // DELETE ROUTE - REMOVE ONE FROM MONGODB 
+    // ******************************************************
+
+    // .get route
+    app.get('/:taskId/delete', async function(req,res){
+        
+        // retrieve the taskId from the params object (Same as in Update route)
+        let taskId = req.params.taskId;
+
+        // Find in MONGODB using the taskId, using the findOne()
+        let dataInExpress = await db.collection( process.env.COLLECTION ).findOne(
+            {
+                '_id' : ObjectId( taskId )
+            }
+        );
+
+        res.render('delete.hbs', {
+            'dataInHbs' : dataInExpress
+        })
+    })
+    // .post route
+    app.post('/:taskId/delete', async function(req,res){
+        
+        // retrieve the taskId from the params object (Same as in Update route)
+        let taskId = req.params.taskId;
+
+        // Find in MONGODB using the taskId, using the findOne()
+        db.collection( process.env.COLLECTION ).deleteOne(
+            {
+                '_id' : ObjectId( taskId )
+            }
+        );
+        
+        res.redirect('/')
+    })
+
+// ============================================
+// ============== 3. L I S T E N ==============
+// ============================================
+    // LISTEN - SET THE PORT TO CONNECT 
+    app.listen(3000, function () {
+        console.log("...We Are Serving...")
+    });
+
+}
+main();
